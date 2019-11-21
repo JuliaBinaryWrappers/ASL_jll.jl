6d7e8f6ced4ceec8a5123d02fa9c4f89bbf63cd0
jlbuild <juliabuildbot@gmail.com>
2019-11-21 00:40:42 +0000

    ASL_jll build 0.1.0+0

---
+++ src/ASL_jll.jl
@@ -0,0 +1,31 @@
+module ASL_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("ae81ac8f-d209-56e5-92de-9978fef736f9"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "ASL", artifacts_toml) for e in artifacts["ASL"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load ASL; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module ASL_jll
