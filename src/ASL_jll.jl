c8089bd9bf3b9f905f91ec41f89fd6db2cb3e184
jlbuild <juliabuildbot@gmail.com>
2026-09-02 10:39:43 +0000

    ASL_jll build 0.1.4+0

--- src/ASL_jll.jl
+++ src/ASL_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("ASL")
-JLLWrappers.@generate_main_file("ASL", UUID("ae81ac8f-d209-56e5-92de-9978fef736f9"))
+JLLWrappers.@generate_main_file("ASL", Base.UUID("ae81ac8f-d209-56e5-92de-9978fef736f9"))
 end  # module ASL_jll
